c32ba3228f902815c883756d5f4b62d8dc437cd0
Dyatlova Maria Sergeevna <145100832+menella00@users.noreply.github.com>
2026-09-23 23:03:27 +0300

    Update run_scalar.py

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -10,16 +10,11 @@ import minitorch
 class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
-        # Первый слой: вход размерности 2 -> скрытый слой размерности hidden_layers
         self.layer1 = Linear(2, hidden_layers)
-        # Второй слой: скрытый слой hidden_layers -> выходной скаляр 1
         self.layer2 = Linear(hidden_layers, 1)
 
     def forward(self, x):
-        # x: список из двух скаляров [x_1, x_2]
-        # Пропускаем через первый слой и применяем ReLU
         h = [y.relu() for y in self.layer1.forward(x)]
-        # Пропускаем через второй слой и применяем Sigmoid к единственному выходу
         return self.layer2.forward(h)[0].sigmoid()
 
 
@@ -44,12 +39,10 @@ class Linear(minitorch.Module):
             )
 
     def forward(self, inputs):
-        # inputs: список скаляров длины in_size
         out = []
         out_size = len(self.bias)
         in_size = len(inputs)
         for j in range(out_size):
-            # y_j = b_j + sum_i(inputs[i] * w_ij)
             val = self.bias[j].value
             for i in range(in_size):
                 val = val + inputs[i] * self.weights[i][j].value
